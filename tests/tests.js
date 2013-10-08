208d97e4b6a23b868ce71c8c01055bd3346efa88
Ryan Grieve <me@ryangrieve.com>
2013-10-08 21:11:17 +0100

    adding test runner and adding require to lib to make it self contained

---
+++ tests/tests.js
@@ -0,0 +1,27 @@
+require.config({
+    baseUrl: '../src',
+    paths: {
+        'underscore': 'lib/underscore'
+    }
+});
+
+require(
+    [
+        "../tests/spec/utils/timer",
+        "../tests/spec/graphics/Spritemap"
+    ],
+    function(){
+        var jasmineEnv = jasmine.getEnv();
+        jasmineEnv.updateInterval = 1000;
+
+        var htmlReporter = new jasmine.HtmlReporter();
+
+        jasmineEnv.addReporter(htmlReporter);
+
+        jasmineEnv.specFilter = function(spec) {
+            return htmlReporter.specFilter(spec);
+        };
+        
+        jasmineEnv.execute();
+    }
+);
