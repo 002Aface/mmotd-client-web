fcd05c335a269ad2f87c5e4df8f0946f9ea8d1ce
Ryan Grieve <me@ryangrieve.com>
2013-10-09 22:57:35 +0100

    Fleshing out base system class to fufill test spec

--- src/ecs/system/Base.js
+++ src/ecs/system/Base.js
@@ -8,9 +8,40 @@ define(
 
             EventEmitter.call(this);
             this.identifier = "BaseSystem";
+            this.components = [];
         };
 
         BaseSystem.prototype = Object.create(EventEmitter.prototype);
+
+        BaseSystem.prototype.register = function(component){
+
+            for (var i = 0; i < this.components.length; i++){
+                if (this.components[i] == component){
+                    throw new Error("Component has already been registered");
+                }
+            }
+            this.components.push(component);
+            this.emit('componentRegistered', component);
+        };
+
+        BaseSystem.prototype.unregister = function(component){
+
+            for (var i = 0; i < this.components.length; i++){
+                if (this.components[i] == component){
+                    break;
+                }
+            }
+            if(i < this.components.length){
+                this.components.splice(i, 1);
+                this.emit('componentUnregistered', component);
+            } else {
+                throw new Error("Component has not been registered");
+            }
+        };
+
+        BaseSystem.prototype.update = function(component){
+
+        };
 
         return BaseSystem;
     }
