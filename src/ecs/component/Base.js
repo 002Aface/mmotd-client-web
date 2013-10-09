0caa0f8bb7be8a9030d46b29ce5f704784277395
Ryan Grieve <me@ryangrieve.com>
2013-10-09 23:42:24 +0100

    ... and implementing it

--- src/ecs/component/Base.js
+++ src/ecs/component/Base.js
@@ -1,16 +1,35 @@
 define(
     [
-        "EventEmitter"
+        "underscore",
+        "EventEmitter",
+        'ecs/SystemsManager'
     ],
-    function(EventEmitter){
+    function(_, EventEmitter, SystemsManager){
 
-        var BaseComponent = function(){
+        var defaultOptions = {
+            autoRegister: true,
+            system: "BaseSystem"
+        };
+
+        var BaseComponent = function(opts){
 
             EventEmitter.call(this);
 
+            this.systemsManager = new SystemsManager();
+            this.opts = _.extend(defaultOptions, opts);
+
+            if(this.opts.autoRegister){
+                this.register();
+            }
         };
 
         BaseComponent.prototype = Object.create(EventEmitter.prototype);
+
+        BaseComponent.prototype.register = function(){
+            var system = this.systemsManager.getSystem(this.opts.system);
+            system.register(this);
+            system.on('updated', _.bind(this.emit, this));
+        };
 
         return BaseComponent;
     }
