a45d91e8908cc3ea7e2db33a847244220f0c8968
Ryan Grieve <me@ryangrieve.com>
2013-10-09 23:42:14 +0100

    fleshing out test spec for base component

--- tests/spec/ecs/component/Base.js
+++ tests/spec/ecs/component/Base.js
@@ -1,14 +1,21 @@
 define(
     [
-        'ecs/component/Base'
+        'ecs/component/Base',
+        'ecs/system/Base',
+        'ecs/SystemsManager'
     ],
-    function(Entity){
+    function(BaseComponent, BaseSystem, SystemsManager){
         describe("The Base E/C component", function(){
 
             var instance;
+            var system;
+            var manager;
 
             beforeEach(function(){
-                instance = new Entity();
+                instance = new BaseComponent({autoRegister: false, system: "BaseSystem"});
+                manager = new SystemsManager();
+                manager.unregisterAll();
+                system = new BaseSystem({autoRegister: true, autoStart: false});
             });
 
             afterEach(function(){
@@ -22,6 +29,39 @@ define(
                 expect(instance.off instanceof Function).toBe(true);
                 expect(instance.emit).toBeDefined();
                 expect(instance.emit instanceof Function).toBe(true);
+            });
+
+            it("should be able to register itself with the configured system", function(){
+                expect(system.components.length).toEqual(0);
+                instance.register();
+                expect(system.components.length).toEqual(1);
+                expect(system.components[0]).toBe(instance);
+            });
+
+            it("should register itself automatically in the constructor if autoRegister is true", function(){
+                expect(system.components.length).toEqual(0);
+
+                var regInstance = new BaseComponent({autoRegister: true, system: "BaseSystem"});
+
+                expect(system.components.length).toEqual(1);
+                expect(system.components[0]).toBe(regInstance);
+            });
+
+            it("should not register itself automatically if autoRegister is false", function(){
+                expect(system.components.length).toEqual(0);
+
+                var regInstance = new BaseComponent({autoRegister: false, system: "BaseSystem"});
+
+                expect(system.components.length).toEqual(0);
+            });
+
+            it("should emit 'updated' when its registered system updates", function(){
+                instance.register();
+                var cbSpy = jasmine.createSpy('cbSpy');
+                instance.on('updated', cbSpy);
+                expect(cbSpy).not.toHaveBeenCalled();
+                system.update();
+                expect(cbSpy).toHaveBeenCalled();
             });
 
         });
