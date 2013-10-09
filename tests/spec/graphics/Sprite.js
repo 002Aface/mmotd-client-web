5a62835042bcca4cbbb4caf2c0e6594a5488f309
Ryan Grieve <me@ryangrieve.com>
2013-10-09 08:24:14 +0100

    adding test for waiting on load

--- tests/spec/graphics/Sprite.js
+++ tests/spec/graphics/Sprite.js
@@ -3,12 +3,21 @@ define(
     function(Sprite){
         describe('The Sprite class', function(){
 
+            var instance, loadSpy;
+
             beforeEach(function(){
-                instance = new Sprite('test_assets/imgs/sprite.png', 32, 32);
+                loadSpy = jasmine.createSpy('loadSpy');
+                instance = new Sprite('test_assets/imgs/sprite.png', 32, 32, loadSpy);
             });
 
             afterEach(function(){
                 instance = undefined;
+            });
+
+            it("should call the passed onload() function after source image is loaded", function(){
+                waitsFor(function(){
+                    return loadSpy.calls.length == 1;
+                }, 'sprite source image to be loaded', 500);
             });
 
         });
