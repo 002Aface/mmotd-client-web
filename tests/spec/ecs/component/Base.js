ae54e0f38f13ef4f224b4959757e138bf3c4f05f
Ryan Grieve <ryan@rehabstudio.com>
2013-10-10 13:59:31 +0100

    updating base component tests for auto registration when attached to an entity

--- tests/spec/ecs/component/Base.js
+++ tests/spec/ecs/component/Base.js
@@ -2,9 +2,10 @@ define(
     [
         'ecs/component/Base',
         'ecs/system/Base',
+        'ecs/entity/Base',
         'ecs/SystemsManager'
     ],
-    function(BaseComponent, BaseSystem, SystemsManager){
+    function(BaseComponent, BaseSystem, BaseEntity, SystemsManager){
         describe("The Base E/C component", function(){
 
             var instance;
@@ -43,21 +44,39 @@ define(
                 expect(system.components[0]).toBe(instance);
             });
 
-            it("should register itself automatically in the constructor if autoRegister is true", function(){
-                expect(system.components.length).toEqual(0);
+            it("should update the 'entity' property when attached to an entity", function(){
+                var entity = new BaseEntity();
+                expect(instance.entity).toBeNull();
+                entity.addComponent(instance);
+                expect(instance.entity).toBe(entity);
+            });
 
+            it("should null the 'entity' property when removed from an entity", function(){
+                var entity = new BaseEntity();
+                entity.addComponent(instance);
+                expect(instance.entity).toBe(entity);
+                entity.removeComponent(instance);
+                expect(instance.entity).toBeNull();
+            });
+
+            it("should register itself automatically when attached to an entity if autoRegister is true", function(){
                 var regInstance = new BaseComponent({autoRegister: true, system: "BaseSystem"});
+                var entity = new BaseEntity();
+                spyOn(regInstance, 'register');
 
-                expect(system.components.length).toEqual(1);
-                expect(system.components[0]).toBe(regInstance);
+                expect(regInstance.register).not.toHaveBeenCalled();
+                entity.addComponent(regInstance);
+                expect(regInstance.register).toHaveBeenCalled();
             });
 
             it("should not register itself automatically if autoRegister is false", function(){
-                expect(system.components.length).toEqual(0);
-
                 var regInstance = new BaseComponent({autoRegister: false, system: "BaseSystem"});
+                var entity = new BaseEntity();
+                spyOn(regInstance, 'register');
 
-                expect(system.components.length).toEqual(0);
+                expect(regInstance.register).not.toHaveBeenCalled();
+                entity.addComponent(regInstance);
+                expect(regInstance.register).not.toHaveBeenCalled();
             });
 
             it("should emit 'updated' when its registered system updates", function(){
